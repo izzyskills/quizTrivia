68489846507f4dfb1f6d001fde07ab098b06e36f
Israel Omola <izzyskills@gmail.com>
2023-03-16 18:04:01 +0100

    added the dashboard page

--- base/views.py
+++ base/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from . import models, forms
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 # Create your views here.
 
 
@@ -9,6 +11,8 @@ def home_view(request):
 
 
 def login_view(request):
+    if request.user.is_authenticated:
+        return redirect('dashboard')
     if request.method == "POST":
         username = request.POST.get('username').lower()
         password = request.POST.get('password')
@@ -16,7 +20,7 @@ def login_view(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            return redirect('home')
+            return redirect('dashboard')
         else:
             return redirect('register')
 
@@ -29,7 +33,7 @@ def register_view(request):
     context = {'userForm': userForm, 'profileForm': profileForm}
     if request.method == "POST":
         userForm = forms.UserCreationForm(request.POST)
-        profileForm = forms.ProfileCreationForm(request.POST)
+        profileForm = forms.ProfileCreationForm(request.POST, request.FILES)
         if userForm.is_valid() and profileForm.is_valid():
             user = userForm.save(commit=False)
             user.username = user.username.lower()
@@ -39,3 +43,11 @@ def register_view(request):
             profile.save()
         return redirect('login')
     return render(request, 'register.html', context)
+
+
+@login_required(login_url='login')
+def dashboard_view(request):
+    user = User.objects.get(id=request.user.id)
+    context = {'user': user}
+
+    return render(request, 'dashboard.html', context)
